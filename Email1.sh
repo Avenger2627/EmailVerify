a6fc8a88d8c5e87d9e2cde4fdf6e8010d39c720a
Avenger2627 <imraninfo97@gmail.com>
2020-10-15 16:07:25 +0530

    Added:This is Email Form

--- Email1.sh
+++ Email1.sh
@@ -1,2 +1,16 @@
-#!/bin/bash -x
-echo "This is the Email Fiel"
+#!/bin/bash +x
+echo "-----Email Pattern-----"
+
+#valid Format: abc.xyz@bridgelabz.co.in
+#madatory part: abc, bridgelabz, co
+
+format='^[a-zA-Z0-9]+@[a-zA-Z0-9]+.[a-z]{2,4}$'
+
+read -p "Enter Email Id: " email
+
+if [[ $email =~ $foramt ]]
+then
+	echo "$email   IS VALID EMAIL."
+else
+	echo "$email    is Invalid Email-Id ???"
+fi
